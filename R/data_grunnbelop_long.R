ce526fed2209ee52d8e2ae4322e085e56c4c73a2
eal024 <e.lamoey@mail.com>
2024-07-22 09:01:05 +0200

    Legger til datasett -- grunnbeløp

---
+++ R/data_grunnbelop_long.R
@@ -0,0 +1,12 @@
+#' The basic amount in the National Insurance Scheme. Grunnbelopet i norwegian.
+#'
+#' The basic amounts in the period 2001 to 2024.
+#' @format ## `grunnbelop_long`
+#' \describe{
+#'   \item{dato_og_ar}{Date when the basic amount is updated}
+#'   \item{grunnbelop_per_ar}{The basic amount, that given year}
+#'   \item{grunnbelop_per_manedr}{The basic amount, monthly}
+#'   \item{gjennomsnitt_per_ar}{The mean amount for a given year. Since G is adjused in may, the average amount is lower than grunnbelop_per_ar}
+#' }
+#' @source <https://www.nav.no/no/nav-og-samfunn/kontakt-nav/utbetalinger/grunnbelopet-i-folketrygden>
+"grunnbelop_long"
